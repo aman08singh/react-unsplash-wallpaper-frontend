1bad6fbbdb3e53803ca2880c1048bc2886196a5b
aman08singh <aman.rajkumar.singh@gmail.com>
2021-05-23 14:16:57 +0530

    feat/fix:added sortBy feature and some ui changes

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -6,6 +6,7 @@ export default function MainPage() {
   const [searchTerm, setSearchTerm] = useState("");
   const [page, setPage] = useState(1);
   const [perPage, setperPage] = useState(10);
+  const [sortBy, setSortBy] = useState("Relevance");
   const [accordionToggle, setAccordionToggle] = useState(false);
   useEffect(async () => {
     if (searchTerm) {
@@ -31,6 +32,28 @@ export default function MainPage() {
     }
   }, [page, perPage]);
 
+  function sortHelper() {
+    if (sortBy === "Relevance") {
+      return data;
+    } else if (sortBy === "Likes: More-To-Less") {
+      const tempData = [...data];
+      tempData.sort((a, b) => b.user.total_likes - a.user.total_likes); //Desending
+      return tempData;
+    } else if (sortBy === "Likes: Less-To-More") {
+      const tempData = [...data];
+      tempData.sort((a, b) => a.user.total_likes - b.user.total_likes); //Ascending
+      return tempData;
+    } else if (sortBy === "Photos: More-To-Less") {
+      const tempData = [...data];
+      tempData.sort((a, b) => b.user.total_photos - a.user.total_photos); //Desending
+      return tempData;
+    } else if (sortBy === "Photos: Less-To-More") {
+      const tempData = [...data];
+      tempData.sort((a, b) => a.user.total_photos - b.user.total_photos); //Ascending
+      return tempData;
+    }
+  }
+
   function onChangeHandler(e) {
     setSearchTerm(e.target.value);
   }
@@ -52,8 +75,7 @@ export default function MainPage() {
       if (page > 1) {
         setData([]);
         setPage(1);
-      }
-      else blankSearchPageOne();
+      } else blankSearchPageOne();
     } else {
       setData([]);
       await apiCall(
@@ -96,30 +118,53 @@ export default function MainPage() {
     }
   }
 
-  function dropDownHelper(e) {
+  function dropDownHelper(e, type) {
     e.preventDefault();
     const { value } = e.target.attributes;
-    setperPage(value.nodeValue);
+    if (type === "per-page-dropdown") setperPage(value.nodeValue);
+    else if (type === "sort-dropdown") setSortBy(value.nodeValue);
   }
 
-  function options() {
-    let content = [];
-    for (let i = 10; i <= 30; i = i + 10) {
-      content.push(
-        <li key={i}>
+  function options(dropdown) {
+    if (dropdown === "per-page-dropdown") {
+      const content = [5, 10, 20, 30];
+      return content.map((item, index) => (
+        <li key={index}>
           <a
             className={
-              parseInt(perPage) === i ? "dropdown-item active" : "dropdown-item"
+              parseInt(perPage) === item
+                ? "dropdown-item active"
+                : "dropdown-item"
             }
-            value={i}
-            onClick={dropDownHelper}
+            value={item}
+            onClick={(e) => dropDownHelper(e, "per-page-dropdown")}
           >
-            {i}
+            {item}
           </a>
         </li>
-      );
+      ));
+    } else if (dropdown === "sort-dropdown") {
+      const content = [
+        "Relevance",
+        "Likes: More-To-Less",
+        "Likes: Less-To-More",
+        "Photos: More-To-Less",
+        "Photos: Less-To-More",
+      ];
+      return content.map((item, index) => (
+        <li key={index}>
+          <a
+            className={
+              sortBy === item ? "dropdown-item active" : "dropdown-item"
+            }
+            value={item}
+            onClick={(e) => dropDownHelper(e, "sort-dropdown")}
+          >
+            {item}
+          </a>
+        </li>
+      ));
     }
-    return content;
   }
 
   return (
@@ -148,8 +193,8 @@ export default function MainPage() {
           </div>
         </div>
         <div className="row">
-          <div className="dropdown col-sm-12 col-md-12 col-lg-12">
-            <label className="form-label me-2">Images per page</label>
+          <div className="dropdown col-6 col-sm-6 col-md-6 col-lg-6 per-page-dropdown">
+            <label className="form-label me-2">Images per page:</label>
             <button
               className="btn btn-primary dropdown-toggle"
               type="button"
@@ -160,14 +205,34 @@ export default function MainPage() {
               {perPage}
             </button>
             <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-              {options()}
+              {options("per-page-dropdown")}
             </ul>
+          </div>
+          <div className="dropdown col-6 col-sm-6 col-md-6 col-lg-6 sort-dropdown">
+            <div className="sort-dropdown-inner-div">
+              <label className="form-label me-2">Sort By:</label>
+              <button
+                className="btn btn-primary dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton2"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                {sortBy}
+              </button>
+              <ul
+                className="dropdown-menu"
+                aria-labelledby="dropdownMenuButton1"
+              >
+                {options("sort-dropdown")}
+              </ul>
+            </div>
           </div>
         </div>
       </form>
       <div className="row">
-        {data.length > 0 ? (
-          data.map((item) => (
+        {sortHelper().length > 0 ? (
+          sortHelper().map((item) => (
             <div key={item.id} className="col-sm-12 col-md-4 img-column">
               <img
                 className="img-fluid img-class w-100"
@@ -188,7 +253,7 @@ export default function MainPage() {
                     <button
                       className="accordion-button accordion-button-arrow"
                       type="button"
-                      aria-expanded={accordionToggle === item.id ? true : false}
+                      // aria-expanded={accordionToggle === item.id ? true : false}
                       value={item.id}
                       name="accordion"
                       onClick={onSubmitHandler}
@@ -206,47 +271,59 @@ export default function MainPage() {
                     aria-labelledby="headingOne"
                     data-bs-parent="#accordionExample"
                   >
-                    {item.user.bio ? (
-                      <div className="accordion-body">
+                    <div className="accordion-body">
+                      {item.user.bio && (
                         <div>
                           <strong>Bio: </strong>
-                          {item.user.bio}
+                          <span className="text-break">{item.user.bio}</span>
                         </div>
-                        {item.user.total_likes ? (
-                          <div>
-                            <strong>Total Likes: </strong>
-                            {item.user.total_likes}
-                          </div>
-                        ) : null}
-                        {item.user.total_photos ? (
-                          <div>
-                            <strong>Total Photos: </strong>
-                            {item.user.total_photos}
-                          </div>
-                        ) : null}
-                        <hr />
+                      )}
+                      {item.user.total_likes.toString() && (
+                        <div>
+                          <strong>Total Likes: </strong>
+                          {item.user.total_likes}
+                        </div>
+                      )}
+                      {item.user.total_photos.toString() && (
+                        <div>
+                          <strong>Total Photos: </strong>
+                          {item.user.total_photos}
+                        </div>
+                      )}
+                      {(item.user.location ||
+                        item.user.instagram_username ||
+                        item.user.twitter_username) && <hr />}
+                      {(item.user.location ||
+                        item.user.instagram_username ||
+                        item.user.twitter_username) && (
                         <div className="social-network-profiles">
-                          {item.user.location ? (
+                          {item.user.location && (
                             <div>
                               <strong>Location: </strong>
-                              {item.user.location}
+                              <span className="text-break">
+                                {item.user.location}
+                              </span>
                             </div>
-                          ) : null}
-                          {item.user.instagram_username ? (
+                          )}
+                          {item.user.instagram_username && (
                             <div>
                               <strong>Instagram: </strong>
-                              {item.user.instagram_username}
+                              <span className="text-break">
+                                {item.user.instagram_username}
+                              </span>
                             </div>
-                          ) : null}
-                          {item.user.twitter_username ? (
+                          )}
+                          {item.user.twitter_username && (
                             <div>
                               <strong>Twitter: </strong>
-                              {item.user.twitter_username}
+                              <span className="text-break">
+                                {item.user.twitter_username}
+                              </span>
                             </div>
-                          ) : null}
+                          )}
                         </div>
-                      </div>
-                    ) : null}
+                      )}
+                    </div>
                   </div>
                 </div>
               </div>
